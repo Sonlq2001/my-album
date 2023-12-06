70b0cf777025865e6dbc212dd27cbba725e1af46
sonle <sonweb2001@gmail.com>
2023-12-06 22:26:40 +0700

    FE: fix api get albums user

--- src/views/my-page/constants/my-page.endpoints.js
+++ src/views/my-page/constants/my-page.endpoints.js
@@ -1,4 +1,5 @@
 export const MyPageEndpoints = {
   PATCH_PROFILE: "/user/:user_id/update",
   GET_USER: "/user/:user_id",
+  GET_ALBUMS_USER: "/album/user",
 };
--- src/views/my-page/screens/MyAlbumList/MyAlbumList.vue
+++ src/views/my-page/screens/MyAlbumList/MyAlbumList.vue
@@ -8,8 +8,8 @@
 
     <button
       :class="[
-        'font-sans text-sm border px-2 py-1 rounded border-[#ddd]  mr-3',
-        // sort.value === initParams.sort ? 'bg-main text-white' : 'bg-gray',
+        'font-sans text-[13px] border px-2 py-[3px] rounded-3xl border-[#ddd]  mr-3',
+        sort.value === initParams.sort ? 'bg-main text-white' : 'bg-gray',
       ]"
       v-for="(sort, index) in LIST_SORT"
       :index="index"
@@ -18,35 +18,80 @@
       {{ sort.label }}
     </button>
 
-    <div class="mt-5 gap-5 flex flex-wrap">
+    <div class="mt-5" v-if="isLoading">Loading...</div>
+    <div class="mt-5 gap-4 flex flex-wrap" v-else>
       <item-album
-        :album="{
-          title: '123',
-          slug: '123',
-          albumAvatar: {
-            imageUrl:
-              'https://cdn.pixabay.com/photo/2023/09/29/11/19/sunrays-8283601_640.jpg',
-          },
-        }"
-        v-for="item in [1, 2, 3, 4, 5]"
-        :key="item"
-        class="item-album mb-0"
+        v-if="
+          myPageStore?.listAlbumsUser && myPageStore?.listAlbumsUser?.length > 0
+        "
+        :album="album"
+        v-for="album in myPageStore.listAlbumsUser"
+        :key="album.id"
+        class="item-album !mb-0 max-h-[180px] h-full"
       />
+      <div v-else>not data</div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref, reactive, watch } from "vue";
+
 import ReturnTo from "@/components/ReturnTo/ReturnTo.vue";
-import AppButton from "@/components/AppButton/AppButton.vue";
-import { LIST_SORT } from "@/constants/app.constants";
+import { LIST_SORT, SORT_VALUE } from "@/constants/app.constants";
 import ItemAlbum from "@/components/ItemAlbum/ItemAlbum.vue";
+import { useMyPageStore } from "@/stores/my-page/my-page.store";
+import { DEFAULT_PAGE, DEFAULT_PER_PAGE } from "@/constants/app.constants";
 
 import { MyPagePaths } from "../../constants/my-page.paths.js";
+
+const myPageStore = useMyPageStore();
+
+const isLoading = ref(true);
+const initParams = reactive({
+  sort: SORT_VALUE.CREATED_DESC,
+  page: DEFAULT_PAGE,
+  perPage: DEFAULT_PER_PAGE,
+});
+
+onMounted(async () => {
+  if (myPageStore.listAlbumsUser?.length) {
+    isLoading.value = false;
+    return;
+  }
+
+  await fetchAlbums();
+  isLoading.value = false;
+});
+
+watch(
+  () => initParams.sort,
+  async () => {
+    await fetchAlbums();
+    isLoading.value = false;
+  }
+);
+
+const fetchAlbums = async (params) => {
+  try {
+    await myPageStore.getAlbumsUserApi({ ...initParams, ...params });
+  } catch (error) {
+    // TODO: handler error
+  }
+};
+
+const handlerSortAlbums = (valueSort) => {
+  initParams.sort = valueSort.value;
+};
 </script>
 
 <style lang="css" scoped>
 .item-album {
-  width: calc((100% / 3) - 13.5px);
+  flex: 1 0 auto;
+  height: 180px;
+}
+
+.item-album:last-child {
+  max-width: 400px;
 }
 </style>
