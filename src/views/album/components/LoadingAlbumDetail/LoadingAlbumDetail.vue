2bc8f06cce5fa1e4c094d6af7f2fe42872e98300
sonle <sonweb2001@gmail.com>
2024-01-31 21:34:02 +0700

    FE: loading album detail

---
+++ src/views/album/components/LoadingAlbumDetail/LoadingAlbumDetail.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="mt-7 flex items-center justify-between">
+    <div class="flex items-center">
+      <div class="animate-pulse">
+        <div class="bg-slate-200 w-[48px] h-[48px] rounded-full" />
+      </div>
+
+      <div class="animate-pulse ml-3">
+        <div class="bg-slate-200 w-[60px] h-[20px] rounded-lg" />
+      </div>
+    </div>
+
+    <div class="flex items-center gap-5">
+      <div class="animate-pulse">
+        <div class="bg-slate-200 rounded-md w-[33px] h-[33px]" />
+      </div>
+
+      <div class="animate-pulse">
+        <div class="bg-slate-200 rounded-3xl w-[120px] h-[41px]" />
+      </div>
+    </div>
+  </div>
+
+  <div class="flex gap-8 justify-center mt-7 max-lg:flex-col">
+    <div class="animate-pulse max-lg:flex max-lg:justify-center">
+      <div
+        class="bg-slate-200 rounded-lg w-full max-w-[600px] h-[380px] max-sm:max-w-[400px]"
+      />
+    </div>
+
+    <div>
+      <div class="mb-3">
+        <div class="animate-pulse ml-3 mb-2">
+          <div class="bg-slate-200 w-full max-w-[80px] h-[25px] rounded-lg" />
+        </div>
+        <div class="animate-pulse ml-3">
+          <div class="bg-slate-200 w-full max-w-[270px] h-[25px] rounded-lg" />
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <div class="animate-pulse ml-3 mb-2">
+          <div class="bg-slate-200 w-full max-w-[80px] h-[25px] rounded-lg" />
+        </div>
+        <div class="animate-pulse ml-3">
+          <div class="bg-slate-200 w-full max-w-[350px] h-[25px] rounded-lg" />
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <div class="animate-pulse ml-3 mb-2">
+          <div class="bg-slate-200 w-full max-w-[80px] h-[25px] rounded-lg" />
+        </div>
+        <div class="animate-pulse ml-3">
+          <div class="bg-slate-200 w-full max-w-[250px] h-[25px] rounded-lg" />
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <div class="animate-pulse ml-3 mb-2">
+          <div class="bg-slate-200 w-full max-w-[80px] h-[25px] rounded-lg" />
+        </div>
+        <div class="animate-pulse ml-3">
+          <div class="bg-slate-200 w-full max-w-[200px] h-[25px] rounded-lg" />
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <div class="animate-pulse ml-3 mb-2">
+          <div class="bg-slate-200 w-full max-w-[80px] h-[25px] rounded-lg" />
+        </div>
+        <div class="animate-pulse ml-3">
+          <div class="bg-slate-200 w-full max-w-[300px] h-[25px] rounded-lg" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped></style>
